e3daf6d9e02e511913e6e197b3780292db603fe1
Ubuntu <ubuntu@ip-10-193-10-20.eu-west-1.compute.internal>
2014-12-23 18:11:37 +0000

    Script for EBI

--- sanger_workflow_test_ebi.sh
+++ sanger_workflow_test_ebi.sh
@@ -1,6 +1,6 @@
 perl bin/sanger_workflow_decider.pl \
 --schedule-force-run \
---seqware-clusters /home/ubuntu/state/instances.json \
+--seqware-clusters conf/empty.json \
 --workflow-version 1.0.2 \
 --bwa-workflow-version 2.6.0 \
 --working-dir ebi \
@@ -8,6 +8,7 @@ perl bin/sanger_workflow_decider.pl \
 --decider-config conf/decider.ini \
 --use-cached-xml \
 #--schedule-whitelist-donor donors_I_want.txt \
+#--seqware-clusters /home/ubuntu/state/instances.json \
 
 #https://gtrepo-etri.annailabs.com  \
 #https://gtrepo-osdc-icgc.annailabs.com
