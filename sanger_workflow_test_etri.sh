99947316ecb130a456b59d3488aac2798a9a5a11
Ubuntu <ubuntu@ip-10-193-10-20.eu-west-1.compute.internal>
2014-12-23 23:37:02 +0000

    tweak ini

--- sanger_workflow_test_etri.sh
+++ sanger_workflow_test_etri.sh
@@ -2,12 +2,13 @@ perl bin/sanger_workflow_decider.pl \
 --schedule-force-run \
 --seqware-clusters conf/cluster.json \
 --bwa-workflow-version 2.6.0 \
---workflow-version 1.0.1 \
+--workflow-version 1.0.2 \
 --working-dir etri \
 --gnos-url  https://gtrepo-etri.annailabs.com \
 --decider-config conf/decider.ini \
 --use-cached-xml \
 #--schedule-whitelist-donor donors_I_want.txt \
+#--seqware-clusters conf/cluster.json \
 
 #https://gtrepo-etri.annailabs.com  \
 #https://gtrepo-osdc-icgc.annailabs.com
